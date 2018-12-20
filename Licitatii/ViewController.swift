679fb463b7d524f362abdbc40da0142458175822
maris <marianatiganus21@gmail.com>
2018-12-20 13:12:17 +0200

    Initial Commit

---
+++ Licitatii/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Licitatii
+//
+//  Created by Moni on 20/12/2018.
+//  Copyright © 2018 Moni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
